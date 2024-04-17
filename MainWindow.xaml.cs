ffbe4d11c192efa32007072f42e7a91e619ea886
RALPH JUSTINE BISENIO <rab0675@students.uc-bcf.edu.ph>
2024-04-17 16:27:15 +0800

    OnClick_MoveNorth and other OnClick is not working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,23 @@ namespace WpfApp2
             DataContext = _gameSession;
         }
 
+        private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+        private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+        private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+        private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+        }
+
         private void InitializeComponent()
         {
             throw new NotImplementedException();
